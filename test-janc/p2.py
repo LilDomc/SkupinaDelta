f3be36361fdd97c27b9e8118ad801c668d2b7b03
Dominik Janc <dominik.janc@student.um.si>
2025-04-06 20:34:58 +0200

    Test-Janc

---
+++ test-janc/p2.py
@@ -0,0 +1,4 @@
+print("Welcome to the program that shows you how JavaScript works. The program will add the two numbers together using a +")
+x = int(input("Enter the first number: "))
+y = int(input("Enter the second number: "))
+print(f"({x}{y})")
